192aaa2c41c8abc57ef5830b05abdaf364219289
Curtis B <ceb2941@gmail.com>
2021-08-17 17:19:40 -0500

    updated sprite_classes.py and asteroids.py
    added projectiles, changed background and player image, added rocket boost

--- asteroids.py
+++ asteroids.py
@@ -1,15 +1,16 @@
 #Asteroid type game made in python using pygame
+#need to fix multiple input actions
 import pygame
 import random
 import sprite_classes
 #buttons used in game
-from pygame.constants import K_RETURN, RLEACCEL, K_ESCAPE, KEYDOWN 
+from pygame.constants import K_RETURN, RLEACCEL, K_ESCAPE, KEYDOWN, K_SPACE
 #create window and initialize
 pygame.init()
-player_size = 50
+PLAYER_SIZE = 50
 width, height = 750, 750
-color_black = (0, 0, 0)
-color_white = (255, 255, 255)
+color_black= (0, 0, 0) #add file for colors
+COLOR_WHITE = (255, 255, 255)
 win = pygame.display.set_mode((width, height)) #creates a game window with given size 
 font = pygame.font.Font('freesansbold.ttf', 32) #font used for all text
 
@@ -29,22 +30,31 @@ class Update_Game():
         pygame.display.flip()
         self.clock.tick(60) 
 
+def create_projectile(player, all_s, projects):
+    p1 = sprite_classes.Projectile(player.get_center_position(), player.get_x(), player.get_y(), PLAYER_SIZE, width, height, player.get_rotation_angle())
+    all_s.add(p1)
+    projects.add(p1)
+
 def main():
     running = True
     s1 = sprite_classes.Player(width, height)
+    projects = pygame.sprite.Group()
     all_s = pygame.sprite.Group()
     all_s.add(s1)
-    Game = Update_Game(all_s, 60, color_white)
+    Game = Update_Game(all_s, 60, color_black)
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN: #exit game if esc key pressed
                 if event.key == K_ESCAPE: 
                     running = False
+                if event.key == K_SPACE:
+                    create_projectile(s1, all_s, projects)
             elif event.type == pygame.QUIT:
                 running = False
         Game.update()
         pressed_key = pygame.key.get_pressed()
         s1.update_position(pressed_key)
+        projects.update()
     pygame.quit()
 
 if __name__ == "__main__":
--- sprite_classes.py
+++ sprite_classes.py
@@ -1,11 +1,14 @@
+from asteroids import COLOR_WHITE
 import pygame
 import math
 #need to account for negative angle
 #just use sin(-x) = -sin(x), cos(-x) = cos (x)
 #import direction_angles 
-
+#need to make a parent class with rotate and out of bounds
 from pygame.constants import RLEACCEL, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE
-from pygame.transform import rotate
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, screen_width, screen_height):
@@ -24,6 +27,18 @@ class Player(pygame.sprite.Sprite):
         self.move_speed_y = 0
         self.x = 0
         self.y = 0
+
+    def get_center_position(self):
+        return self.center_position
+    
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_rotation_angle(self):
+        return self.rotation_angle
 
     def rotate(self, direction):
         if direction == "right":
@@ -49,18 +64,70 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom > self.screen_height:
             self.rect.move_ip(0, -self.screen_height) 
 
+    def check_keys(self, pressed_key):
+        if pressed_key[K_RIGHT]:
+            self.rotate("right")
+        if pressed_key [K_LEFT]:
+            self.rotate("left")
+        if pressed_key [K_UP]:
+            self.move_speed_x += 0.2*self.x
+            self.move_speed_y += 0.2*self.y
+            self.surf1 = pygame.image.load("player_sprite2.png").convert()
+            self.surf1 = pygame.transform.rotate(self.surf1, self.rotation_angle)
+            self.surf1.set_colorkey((0, 0, 0), RLEACCEL)
+        else:
+            self.surf1 = pygame.image.load("player_sprite1.png").convert()
+            self.surf1 = pygame.transform.rotate(self.surf1, self.rotation_angle)
+            self.surf1.set_colorkey((0, 0, 0), RLEACCEL)
+
     def update_position(self, pressed_key):
+        self.x = math.sin(self.rotation_angle*math.pi/180) #get sin value for x term based on rotation angle 
+        self.y = math.cos(self.rotation_angle*math.pi/180) #get cos value for y term
         self.out_of_bounds()
         self.rect.move_ip(-self.move_speed_x,-self.move_speed_y)
         self.center_position = [self.rect.centerx, self.rect.centery] #update position after moving
         #x = direction_angles.sin_angle[self.rotation_angle] #need to account for negative angles
         #y = direction_angles.cos_angle[self.rotation_angle]
-        if pressed_key[K_RIGHT]:
-            self.rotate("right")
-        elif pressed_key[K_LEFT]:
-            self.rotate("left")
-        elif pressed_key[K_UP]:
-            self.x = math.sin(self.rotation_angle*math.pi/180)
-            self.y = math.cos(self.rotation_angle*math.pi/180)
-            self.move_speed_x += 0.2*self.x
-            self.move_speed_y += 0.2*self.y
+        self.check_keys(pressed_key)
+
+class Projectile(pygame.sprite.Sprite):
+    def __init__(self, center_position, player_x, player_y, player_size, screen_width, screen_height, rotation_angle):
+        super(Projectile, self).__init__()
+        self.surf1 = pygame.Surface((2, 2))
+        self.surf1.fill(COLOR_WHITE)
+        self.player_size = player_size
+        self.player_x = player_x #maybe create tuples to clean up a bit
+        self.player_y = player_y
+        self.x = center_position[0] - 30 * player_x #puts projectile at tip of player based on rotation
+        self.y = center_position[1] - 30 * player_y
+        self.rect = self.surf1.get_rect(center = (self.x, self.y)) 
+        self.orientation_flag = 0
+        self.p_speed = 15
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.travel_distance = 0
+        self.move_speed_x = 0
+        self.move_speed_y = 0
+        self.rotation_angle = rotation_angle #if an image is added could rotate
+
+    def out_of_bounds(self):
+        if self.rect.right > self.screen_width:
+            self.rect.move_ip(-self.screen_height, 0)
+        if self.rect.left < 0:
+            self.rect.move_ip(self.screen_height, 0)
+        if self.rect.top < 0:
+            self.rect.move_ip(0, self.screen_height)
+        if self.rect.bottom > self.screen_height:
+            self.rect.move_ip(0, -self.screen_height)
+
+    def update(self):
+        #change firing position based off player orientation
+        #keep updating position until out of bounds
+        self.out_of_bounds()
+        if self.travel_distance < self.screen_width:
+            self.move_speed_x = -10*self.player_x #negative value moves in positive x direction
+            self.move_speed_y = -10*self.player_y
+            self.rect.move_ip(self.move_speed_x, self.move_speed_y)
+            self.travel_distance += self.p_speed
+        else:
+            self.kill()
